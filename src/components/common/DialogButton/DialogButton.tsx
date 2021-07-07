86a05f561da5865e1ecb89a09bf1e021d6690f0a
Andrzej <andrzej.pruszynski90@gmail.com>
2021-07-07 15:36:11 +0200

    dev/fit-9 -

--- src/components/common/DialogButton/DialogButton.tsx
+++ src/components/common/DialogButton/DialogButton.tsx
@@ -1,25 +1,10 @@
 import React, { useState } from 'react';
 import {useRouter} from 'next/router';
-import {useSelector} from 'react-redux';
 
 import useStyles from './DialogButton.styles';
 import classnames from 'classnames';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-
-import WorkoutDetails from '../../WorkoutDetails'
-
-// interface DialogButtonProps {
-//      children: React.ReactNode
-// }
-// 
-
-// <DialogButton label="Click">
-//      <div>
-//          content
-//      </div>
-// </DialogButton>
 
 interface DialogButtonProps {
     label?: string;
