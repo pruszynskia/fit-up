6499e7cf5e5b8de9c40a1e20024ff86ffbe38a72
Andrzej <andrzej.pruszynski90@gmail.com>
2021-09-03 13:03:53 +0200

    dev/fit-13 - create serverside routes

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -4,5 +4,9 @@ import controllers from '../controllers/controllers';
 const router = express.Router();
 
 router.get("/workouts",controllers.getAllWorkouts);
+router.get("/workouts/:id", controllers.getWorkoutById);
+router.post("/workouts", controllers.addWorkout);
+router.put("/workouts", controllers.updateWorkout);
+router.delete("workouts", controllers.deleteWorkout);
 
 export default router;
