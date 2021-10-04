a5ed6964ddab0949140667d914556380ea666d79
Andrzej Pruszyński <andrzej.pruszynski90@gmail.com>
2021-10-04 19:20:48 +0200

    dev/fit-14 - fix reducer test

--- client/src/redux/reducers/index.test.ts
+++ client/src/redux/reducers/index.test.ts
@@ -14,42 +14,42 @@ describe('root reducer', () => {
         const addWorkoutAction = {
             type: actions.addWorkoutTest
         };
-        expect(rootReducer( {workout: [], workoutDays: [], test: [] }, addWorkoutAction)).toEqual({ workout: [] })
+        expect(rootReducer( {workout: [], workoutDays: [], test: [] }, addWorkoutAction)).toEqual({workout: [], workoutDays: [], test: [] })
     });
     
     it('should handle add workout test action', () => {
         const deleteWorkoutAction = {
             type: actions.deleteWorkoutTest
         };
-        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, deleteWorkoutAction)).toEqual({ workout: [] })
+        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, deleteWorkoutAction)).toEqual({workout: [], workoutDays: [], test: [] })
     });
     
     it('should handle add workout test action', () => {
         const editWorkoutAction = {
             type: actions.editWorkoutTest
         };
-        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, editWorkoutAction)).toEqual({ workout: [] })
+        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, editWorkoutAction)).toEqual({workout: [], workoutDays: [], test: [] })
     });
     
     it('should handle add workout day test action', () => {
         const addWorkoutDayAction = {
             type: actions.addWorkoutDayTest
         };
-        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, addWorkoutDayAction)).toEqual({ workoutDay: [] })
+        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, addWorkoutDayAction)).toEqual({ workout: [], workoutDays: [], test: []  })
     });
     
     it('should handle add workout day test action', () => {
         const deleteWorkoutDayAction = {
             type: actions.deleteWorkoutDayTest
         };
-        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, deleteWorkoutDayAction)).toEqual({ workoutDay: [] })
+        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, deleteWorkoutDayAction)).toEqual({ workout: [], workoutDays: [], test: [] })
     });
     
     it('should handle add workout day test action', () => {
         const editWorkoutDayAction = {
             type: actions.editWorkoutDayTest
         };
-        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, editWorkoutDayAction)).toEqual({ workoutDay: [] })
+        expect(rootReducer({ workout: [], workoutDays: [], test: [] }, editWorkoutDayAction)).toEqual({ workout: [], workoutDays: [], test: [] })
     });
 
     
