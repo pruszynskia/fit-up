24ad4d3fb4bd96b0e4ddd1a0de46320e5a5c6451
jan <jan.pruszynski@agito1.com>
2021-10-07 09:59:40 +0200

    amended jest config and setup files

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,19 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testEnvironment: "jsdom",
+  transform: {
+    /* Use babel-jest to transpile tests with the next/babel preset
+    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
 };
--- client/src/components/WorkoutDayForm/WorkoutDayForm.test.tsx
+++ client/src/components/WorkoutDayForm/WorkoutDayForm.test.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import TestRenderer from 'react-test-renderer';
-import moment from 'moment';
-import WorkoutDayForm from './WorkoutDayForm';
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import moment from "moment";
+import WorkoutDayForm from "./WorkoutDayForm";
 
 interface WorkoutDayForm {
-    // data?: WorkoutDayDetails;
-    date: moment.Moment;
-    handleCloseF: () => void
+  // data?: WorkoutDayDetails;
+  date: moment.Moment;
+  handleCloseF: () => void;
 }
 
-describe.skip("Workout day form", () => {
+describe("Workout day form", () => {
+  it("Should render as expected", () => {
+    let date = moment();
+    let handleCloseF = () => handleCloseF;
 
-    it("Should render as expected", () => {
-        let date = moment();
-        let handleCloseF = () => handleCloseF
-
-        const tree = TestRenderer
-            .create(
-                <WorkoutDayForm date={date} handleCloseF={handleCloseF}/>
-            )
-            .toJSON();
-            expect(tree).toMatchSnapshot();
-    });
+    const tree = TestRenderer.create(
+      <WorkoutDayForm date={date} handleCloseF={handleCloseF} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
